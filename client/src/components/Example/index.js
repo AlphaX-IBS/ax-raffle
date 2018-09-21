4e04f367ad4dba8130d55e72e95349441c328561
tobyio <toby@greenx.network>
2018-09-21 18:00:48 +0700

    Init project, copy UI from branch layout, using bootstrap and reactstrap.

---
+++ client/src/components/Example/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const Example = props => {
+  return (
+    <p>
+      This is a tooltip{" "}
+      <Button color="danger">Example</Button>
+    </p>
+  );
+};
+
+export default Example;
---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { IntlProvider } from "react-intl";
+
+ReactDOM.render(
+  <IntlProvider>
+    <App />
+  </IntlProvider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
